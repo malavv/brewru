b180ce1bdd767692df529402f8536801cfa252da
Maxime Lavigne (malavv) <duguigne@gmail.com>
2015-12-31 17:05:13 -0500

    Verified that vulcanized worked

--- src/builder/gulpfile.js
+++ src/builder/gulpfile.js
@@ -4,12 +4,17 @@
 
   var
     bower = require('gulp-bower'),
-    sync = require('browser-sync').create(),
-    gulp = require('gulp'),
     concat = require('gulp-concat'),
+    del  = require('del'),
+    gulp = require('gulp'),
+    merge = require('merge-stream'),
+    replace = require('gulp-replace'),
+    size = require('gulp-size'),
     sourcemaps = require('gulp-sourcemaps'),
+    sync = require('browser-sync').create(),
     ts = require('gulp-typescript'),
-    del  = require('del');
+    tsLint = require('gulp-tslint'),
+    vulcanize = require('gulp-vulcanize');
 
   var paths = {
     pages: ['elements/**/*.html', 'elements/*.html', 'index.html'],
@@ -24,26 +29,6 @@
     declaration: true,
     noImplicitAny: true
   });
-
-  var
-    replace = require('gulp-replace'),
-    $ = require('gulp-load-plugins')(),
-    clean = require('gulp-clean'),
-    del  = require('del'),
-    concat = require('gulp-concat'),
-    sync = require('browser-sync'),
-    connect = require('gulp-connect'),
-    runSequence = require('run-sequence'), 
-    reload = sync.reload,
-    rename = require("gulp-rename"),
-    replace = require("gulp-replace"),
-    path = require('path'),
-    merge = require('merge-stream'),
-    glob = require('glob'),
-    ts = require('gulp-typescript'),
-    tsLint = require('gulp-tslint'),
-    IOWA = require('./package.json').iowa,
-    historyApiFallback = require('connect-history-api-fallback');
 
   gulp.task('default', ['dev']);
 
@@ -98,9 +83,10 @@
 
   /** Serves the distribution version. */
   gulp.task('serve:dist', ['dist'], function () {
-    browserSync.init({ server: { baseDir: 'dist/' }});
-    gulp.watch(paths.allDist).on('change', browserSync.reload);
+    sync.init({ server: { baseDir: 'dist/' }});
+    gulp.watch(paths.allDist).on('change', sync.reload);
   });
+
   /** Allows serving the static pages right from here. */
   gulp.task('serve:dev', [], function () {
     sync.init({ server: { baseDir: '.' }});
@@ -109,105 +95,10 @@
     gulp.watch(['elements/**/*.ts'], ['compile:ts']);
   });
 
-
-
-  /* Compiles the polymer elements that will use the brewru library to implement the recipe builder functionalities. */
-  gulp.task('ts', function () {
-    return gulp.src('elements/**/*.ts')
-      .pipe(ts({
-        noImplicitAny: true,
-        target: 'es5'
-        }))
-      .js
-      .pipe(gulp.dest('elements'));
-  });
-
-  // Copy All Files At The Root Level
-  gulp.task('copy', function () {
-    var bower = gulp.src([
-      'bower_components/**/*'
-    ]).pipe(gulp.dest('dist/bower_components'));
-
-    var elements = gulp.src(['elements/**/*.html'])
-      .pipe(gulp.dest('dist/elements'));
-      
-    var scripts = gulp.src(['elements/**/*.js'])
-      .pipe(gulp.dest('dist/elements'));
-      
-    var styles = gulp.src(['elements/**/*.css'])
-      .pipe(gulp.dest('dist/elements'));
-      
-    var vulcanized = gulp.src(['elements/elements.html'])
-      .pipe($.rename('elements.vulcanized.html'))
-      .pipe(gulp.dest('dist/elements'));
-
-    return merge(bower, scripts, styles, elements, vulcanized)
-      .pipe($.size({title: 'copy'}));
-  });
-
-  // Lint JavaScript
-  gulp.task('lint', function () {
-    return gulp.src('elements/**/*.ts')
-      .pipe(reload({stream: true, once: true}))
-      .pipe(tsLint())
-      .pipe(tsLint.report('verbose'));
-  });
-
   // Optimize Library
   gulp.task('lib', function () {
     return gulp.src('lib/**/*')
       .pipe(gulp.dest('dist/lib'))
       .pipe($.size({title: 'lib'}));
   });
-
-  // Scan Your HTML For Assets & Optimize Them
-  gulp.task('html', function () {
-    var assets = $.useref.assets({searchPath: ['dist']});
-
-    return gulp.src(['**/*.html', '!{elements,test}/**/*.html'])
-      // Replace path for vulcanized assets
-      .pipe($.if('*.html', $.replace('elements/elements.html', 'elements/elements.vulcanized.html')))
-      .pipe(assets)
-      // Concatenate And Minify JavaScript
-      .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
-      // Concatenate And Minify Styles
-      // In case you are still using useref build blocks
-      .pipe($.if('*.css', $.cssmin()))
-      .pipe(assets.restore())
-      .pipe($.useref())
-      // Minify Any HTML
-      .pipe($.if('*.html', $.minifyHtml({
-        quotes: true,
-        empty: true,
-        spare: true
-      })))
-      // Output Files
-      .pipe(gulp.dest('dist'))
-      .pipe($.size({title: 'html'}));
-  });
-
-  // Vulcanize imports
-  gulp.task('vulcanize', function () {
-    var DEST_DIR = 'dist/elements';
-
-    return gulp.src('dist/elements/elements.vulcanized.html')
-      .pipe($.vulcanize({
-        stripComments: true,
-        inlineCss: true,
-        inlineScripts: true
-      }))
-      .pipe(gulp.dest(DEST_DIR))
-      .pipe($.size({title: 'vulcanize'}));
-  });
-
-  gulp.task('default', ['clean'], function(cb) {
-    runSequence(
-      ['copy', 'sass', 'ts'],
-      ['lint', 'html', 'lib'],
-      'vulcanize',
-      cb
-    );
-  });
-
-
 })();
